dd4dcb2dab936caeb1d2bf13243c1a1630ca53b1
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2022-05-29 11:32:33 +0800

    Update secret and debug for registration_data['verification_url']

--- app/services/verify_registration.rb
+++ app/services/verify_registration.rb
@@ -13,8 +13,9 @@ module WiseTube
     end
 
     def call(registration_data)
-      registration_token = SecureMessage.encrypt(registration_data)
+      puts("===============================")
       puts(registration_data)
+      registration_token = SecureMessage.encrypt(registration_data)
       registration_data['verification_url'] =
         "#{@config.APP_URL}/auth/register/#{registration_token}"
 

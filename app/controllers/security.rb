f1264a19eff7833fb04ddc1ab1437b886cb82244
NianHung <joanne.lee@iss.nthu.edu.tw>
2022-06-11 15:15:31 +0800

    update secuity.rb file

--- app/controllers/security.rb
+++ app/controllers/security.rb
@@ -38,3 +38,32 @@ module Credence
       config.x_permitted_cross_domain_policies = 'none'
       config.referrer_policy = 'origin-when-cross-origin'
 
+      # note: single-quotes needed around 'self' and 'none' in CSPs
+      # rubocop:disable Lint/PercentStringArray
+      config.csp = {
+        report_only: false,
+        preserve_schemes: true,
+        default_src: %w['self'],
+        child_src: %w['self'],
+        connect_src: %w[wws:],
+        img_src: %w['self'],
+        font_src: %w['self'] + FONT_SRC,
+        script_src: %w['self'] + SCRIPT_SRC,
+        style_src: %W['self'] + STYLE_SRC,
+        form_action: %w['self'],
+        frame_ancestors: %w['none'],
+        object_src: %w['none'],
+        block_all_mixed_content: true,
+        report_uri: %w[/security/report_csp_violation]
+      }
+      # rubocop:enable Lint/PercentStringArray
+    end
+
+    route('security') do |routing|
+      # POST security/report_csp_violation
+      routing.post 'report_csp_violation' do
+        App.logger.warn "CSP VIOLATION: #{request.body.read}"
+      end
+    end
+  end
+end

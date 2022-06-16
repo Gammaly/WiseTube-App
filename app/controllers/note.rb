18a833d67efb0f5048f7fc771bc4fd8c2de92316
schwarz-tw <schwarz.tsai@gmail.com>
2022-06-16 23:56:34 +0800

    finish basic note

---
+++ app/controllers/note.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative './app'
+require 'pp'
+
+# rubocop:disable Metrics/BlockLength
+module WiseTube
+  # Web controller for WiseTube API
+  class App < Roda
+    route('note') do |routing|
+      routing.redirect '/auth/login' unless @current_account.logged_in?
+
+      routing.on String do |link_id|
+        @link_route = "/video/#{link_id}"
+        # POST api/v1/note/[ID]
+        routing.post do
+          note = routing.params['video-note']
+          puts "note: #{note}"
+          note = AddNote.new(App.config).call(
+            current_account: @current_account,
+            link_id: link_id,
+            note: note
+          )
+
+          routing.redirect @link_route
+        rescue StandardError => e
+          puts "ADD NOTE ERROR: #{e.inspect}"
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength

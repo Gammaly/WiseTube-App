eea95fa4986307783509baebf7d1047a1378993c
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2022-06-19 21:19:46 +0800

    Fix merge bug in getlink

--- spec/integration/service_get_account_detail_spec.rb.rb
+++ spec/integration/service_get_account_detail_spec.rb.rb
@@ -6,7 +6,7 @@ describe 'Test GetLink Service Objects' do
   before do
     @credentials = { username: 'soumya.ray', password: 'mypa$$w0rd' }
     @link_id = 'f91a942a-6371-4e36-bfb4-bc1c54f535e8'
-    @api_link = { title: '國立清華大學 服務科學研究所', description: 'ISS NTHU Website' }
+    @api_playlist = { title: '國立清華大學 服務科學研究所', description: 'ISS NTHU Website' }
   end
 
   after do
@@ -30,16 +30,16 @@ describe 'Test GetLink Service Objects' do
       get_link_file = 'spec/fixtures/get_link.json'
       get_link_return_json = File.read(get_link_file)
 
-      WebMock.stub_request(:get, "#{API_URL}/links/#{@link_id}")
+      WebMock.stub_request(:get, 'http://localhost:3000/api/v1/playlists/f91a942a-6371-4e36-bfb4-bc1c54f535e8')
              .with(headers: { 'Authorization' => "Bearer #{auth_os.auth_token}" })
              .to_return(status: 200, body: get_link_return_json, headers: { 'content-type' => 'application/json' })
 
-      link = WiseTube::GetLink.new(APP_CONFIG).call(auth_os, @link_id)
-      link = link['attributes']
+      playlists_list = WiseTube::GetLink.new(APP_CONFIG).call(auth_os, @link_id)
+      playlists_list = playlists_list['attributes']
 
-      _(link).wont_be_nil
-      _(link['title']).must_equal @api_link[:title]
-      _(link['description']).must_equal @api_link[:description]
+      _(playlists_list).wont_be_nil
+      _(playlists_list['title']).must_equal @api_playlist[:title]
+      _(playlists_list['description']).must_equal @api_playlist[:description]
     end
   end
 end

8be6fa025fc35b3ca299afe5ceaef18c782ec582
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2022-06-19 21:52:30 +0800

    Add spec for SecureMessage

--- spec/unit/string_security_spec.rb
+++ spec/unit/string_security_spec.rb
@@ -3,11 +3,32 @@
 require_relative '../spec_helper'
 require 'webmock/minitest'
 
-describe 'Test StringSecurity' do
-  it 'HAPPY: should be able to judge the entropy of a string' do
-    string = 'UnitTest'
-    string_security = StringSecurity.entropy(string)
+describe 'Test secure lib' do
+  describe 'Test StringSecurity' do
+    it 'HAPPY: should be able to judge the entropy of a string' do
+      string = 'UnitTest'
+      string_security = StringSecurity.entropy(string)
 
-    _(string_security).wont_be_nil
+      _(string_security).wont_be_nil
+    end
+  end
+
+  describe 'Test SecureMessage' do
+    it 'HAPPY: should be able to encrypt a message' do
+      message = 'SEC 2022'
+      message_encrypted = SecureMessage.encrypt(message)
+
+      _(message_encrypted).wont_be_nil
+      _(message_encrypted).wont_equal message
+    end
+
+    it 'HAPPY: should be able to decrypt a message' do
+      message = 'SEC 2022'
+      message_encrypted = SecureMessage.encrypt(message)
+      message_decrypted = SecureMessage.decrypt(message_encrypted)
+
+      _(message_decrypted).wont_be_nil
+      _(message_decrypted).must_equal message
+    end
   end
 end
